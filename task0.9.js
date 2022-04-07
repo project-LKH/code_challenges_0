dd72f60ed550bb3f37e2f701d50003f10d3ebfbf
Liam <projectlkh63@gmail.com>
2022-04-07 10:32:13 +0200

    reduced code using 'RegEx'

--- task0.9.js
+++ task0.9.js
@@ -1,14 +1,6 @@
 function checkForVowels(str) {
-    const answer = [];
-    const vowels = ["a","e","i","o","u","A","E","I","O","U"];
-    for (let i=0;i<str.length; i+=1){
-        for (let j=0;j<vowels.length; j+=1){
-            if (str[i]==vowels[j]){
-                answer.push(str[i]);
-            }
-        }
-    }
-    const x = Array.from(new Set(answer)); // to re-create an array with no duplicates
-    console.log("Vowels : "+x);
+  let regEx = /[aeiou]/g;
+  const y = Array.from(new Set(str.toLowerCase().match(regEx)));
+  console.log(`vowels : ${y.toString().replace(/,/g, ", ")}`);
 }
-checkForVowels("what are the vowels in this sentence?");
+checkForVowels("what Are the vowEls in thIs sentence?");
